8e27af69d26af51f257659b523e563765bf599d2
Gayatrid31 <gayatrideshmukh2005@gmail.com>
2025-10-15 14:47:09 +0530

    Update java1(switch case).sql

--- create or replace trigger updatemaster before update or delete on clientmaster for each row BEGIN insert into Audit_t (srno, name) values (:old.srno, :old.name); END; /java1(switch case).sql
+++ create or replace trigger updatemaster before update or delete on clientmaster for each row BEGIN insert into Audit_t (srno, name) values (:old.srno, :old.name); END; /java1(switch case).sql
@@ -5,7 +5,7 @@ public class databaseconnect {
 public static void main(String[] args) {
 try {
 Class.forName("com.mysql.cj.jdbc.Driver");
-Connection con=DriverManager.getConnection("jdbc:mysql://localhost/collegedemos1","root","Prajakta@2004");
+Connection con=DriverManager.getConnection("jdbc:mysql://localhost/collegedemos1","root","gayatri@2005");
 			Statement stm=con.createStatement();
 			Scanner sc = new Scanner(System.in);
 			int choice;
